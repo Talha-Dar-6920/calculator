e98f032825e7f7336667de5629fb8c4b34840b23
cyber-talhaaa <talhadar13579@gmail.com>
2021-08-27 12:23:07 +0500

    Refactored Some Code

--- script.js
+++ script.js
@@ -3,23 +3,21 @@ let value = '';
 let operator = '';
 let input = document.querySelector('.input');
 
-document.querySelectorAll('button').forEach(function(el) {
-    el.addEventListener('click', function(e) {
-        if (parseInt(e.target.innerText) || parseInt(e.target.innerText) === 0 || e.target.innerText === '.') {
-            value += e.target.innerText;
-            input.innerHTML = value;
-        } else if (e.target.innerText === '=') {
-            input.innerHTML = eval(previous + operator + parseFloat(value));
-        } else if (e.target.innerText.toLowerCase() == 'c') {
-            input.innerText = '';
-            value = '';
-            previous = 0;
-            operator = '';
-        } else if (e.target.value === '+' || '-' || '*' || '/' || '%') {
-            input.innerText = '';
-            previous = value;
-            value = '';
-            operator = e.target.value;
-        }
+document.querySelectorAll('button').forEach((el) =>
+    el.addEventListener('click', (e) => {
+        parseInt(e.target.innerText) ||
+            parseInt(e.target.innerText) === 0 ||
+            e.target.innerText === '.' ?
+            ((value += e.target.innerText), (input.textContent = value)) :
+            e.target.innerText === '=' ?
+            (input.textContent = eval(previous + operator + parseFloat(value))) :
+            e.target.innerText.toLowerCase() == 'c' ?
+            ((input.innerText = ''), (value = ''), (previous = 0), (operator = '')) :
+            e.target.value === '+' || '-' || '*' || '/' || '%' ?
+            ((input.innerText = ''),
+                (previous = eval(previous + operator + parseFloat(value))),
+                (value = ''),
+                (operator = e.target.value)) :
+            '';
     })
-})
+);
